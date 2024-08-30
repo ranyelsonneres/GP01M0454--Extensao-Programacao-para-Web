1423363ab297c790bb0048e373a7aa6fd0d3e92d
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2024-08-30 11:25:32 -0300

    Update script.js

--- Aula_30_08_2024/script.js
+++ Aula_30_08_2024/script.js
@@ -6,37 +6,37 @@ console.log("Funcionou!!");
 
 //For
 //Ex1: Exibir números de 1 a 5
-// document.write("Números de 1 a 5:<br>");
-// for (let i=1; i<=5;i++){
-//     //console.log(i);
-//     document.write(i + "<br>");
-// }
+ document.write("Números de 1 a 5:<br>");
+for (let i=1; i<=5;i++){
+    console.log(i);
+    document.write(i + "<br>");
+ }
 
 //Ex2: Criar um programa que solicita ao usuário uma qtde de numeros
-// let quantidade = parseInt(prompt("Qtde de números: ")); //forçar a troca de tipo
-// console.log(typeof quantidade);
-// for (let i=1; i<=quantidade; i++){
-//     document.write(i + "<br>");
-// }
+ let quantidade = parseInt(prompt("Qtde de números: ")); //forçar a troca de tipo
+ console.log(typeof quantidade);
+ for (let i=1; i<=quantidade; i++){
+     document.write(i + "<br>");
+}
 
 //Ex3: solicitar um número e calcular a soma de todos os números
-// let limite = parseInt(prompt("Digite o limite: "));
-// let soma = 0;
-// for (let i=1; i<=limite; i++){
-//     let numero = parseInt(prompt("Numero: "));
-//     document.write("Número: " + numero + "<br>");
-//     soma += numero;
-// }
-// document.write("Soma dos números: " + soma);
+ let limite = parseInt(prompt("Digite o limite: "));
+ let soma = 0;
+ for (let i=1; i<=limite; i++){
+     let numero = parseInt(prompt("Numero: "));
+     document.write("Número: " + numero + "<br>");
+     soma += numero;
+ }
+document.write("Soma dos números: " + soma);
 
 //Exemplos com o While
 //Exemplo 1:
-// let contador = 1;
-// document.write("Números de 1 a 5:<br>");
-// while (contador<=5){
-//     document.write(contador + "<br>");
-//     contador++;
-// }
+ let contador = 1;
+ document.write("Números de 1 a 5:<br>");
+ while (contador<=5){
+     document.write(contador + "<br>");
+     contador++;
+ }
 
 //Exemplo 2: somar os números positivos
 let soma = 0;
